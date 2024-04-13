d480002b5dccd7a12ff07bdb5b4b0b496dcbc6db
hrisz <harissaefuloh@gmail.com>
2024-04-13 13:07:07 +0700

    mod: add scramble text animation

---
+++ assets/js/main-scramble.js
@@ -0,0 +1,89 @@
+"use strict";
+
+class TextScramble {
+  constructor(el) {
+    this.el = el;
+    this.chars = "!<>-_\\/[]{}—=+*^?#___";
+    this.update = this.update.bind(this);
+  }
+
+  setText(newText) {
+    const oldText = this.el.innerText;
+    const length = Math.max(oldText.length, newText.length);
+    const promise = new Promise((resolve) => (this.resolve = resolve));
+    this.queue = [];
+
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || "";
+      const to = newText[i] || "";
+      const start = Math.floor(Math.random() * 20);
+      const end = start + Math.floor(Math.random() * 20);
+      this.queue.push({
+        from,
+        to,
+        start,
+        end,
+      });
+    }
+
+    cancelAnimationFrame(this.frameRequest);
+    this.frame = 0;
+    this.update();
+    return promise;
+  }
+
+  update() {
+    let output = "";
+    let complete = 0;
+
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i];
+
+      if (this.frame >= end) {
+        complete++;
+        output += to;
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar();
+          this.queue[i].char = char;
+        }
+
+        output += `<span class="dud">${char}</span>`;
+      } else {
+        output += from;
+      }
+    }
+
+    this.el.innerHTML = output;
+
+    if (complete === this.queue.length) {
+      this.resolve();
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update);
+      this.frame++;
+    }
+  }
+
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)];
+  }
+}
+
+const phrases = [
+  "Journey Beyond The Horizon with Us",
+  "Discover New Realms by Our Side",
+  "Chart Uncharted Paths",
+  "Embark on Limitless Adventures",
+];
+const el = document.querySelector(".textfx");
+const fx = new TextScramble(el);
+let counter = 0;
+
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 5000);
+  });
+  counter = (counter + 1) % phrases.length;
+};
+
+next();
